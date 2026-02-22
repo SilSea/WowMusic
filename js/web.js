65c0ed05da3efd26a526847a40b36edf4c6c84b9
Nattawut Pulket <nattawutpoolkhet@outlook.com>
2026-02-22 22:21:49 +0700

    init commit

---
+++ js/web.js
@@ -0,0 +1,49 @@
+// ListMusic
+var listname = {
+    m87: "M87",
+    ireallywanttostayatyourhouse: "I Really Want To Stay At Your House",
+    GeorgeKarizakisNoRiderSystem: "George Karizakis No Rider System",
+    volcano: "VOLCANO",
+    TRUSTLAST: "TRUSTLAST",
+    Cherryish: "Cherry-ish!",
+    TsumitoBatsutoAngura: "Tsumi to Batsu to Angura",
+    Kaibutsu: "Kaibutsu",
+    GenesisofAquarion: "Genesis of Aquarion",
+    OneLastKiss: "『One Last Kiss』"
+}
+var listmusic = {
+    m87: "185pFuejPuQnO-CVF9vAmFdaBfh7mVKRk",
+    ireallywanttostayatyourhouse: "11sZ26OGW9uj4nlC_UE60s1rAXVBbito9",
+    GeorgeKarizakisNoRiderSystem: "1HaEAkYRxaqDj0lOfKU2kvo_mvgR--xEP",
+    volcano: "1CAUSEH21amtLshMXng58UOJW2XtZ1dlm",
+    TRUSTLAST: "1NfaAveIVgDLfa-Gksw1iRRB5LwDPCWxm",
+    Cherryish: "1R7rwX8DnSto6b5oFDeWTARMW09KRduLb",
+    TsumitoBatsutoAngura: "1LWgG0WUyHdoEmqHY4hmYsNygB3Mj-sg9",
+    Kaibutsu: "1YAoc5sQZbJfkEpF5pvo8crgkfj93TCpa",
+    GenesisofAquarion: "1o87_CbEdNXAgngoqzofCZkD7fHIqpDYv",
+    OneLastKiss: "1E-RNORXjX2wVjV2Vn0AU9V_lflytfytm"
+}
+var listvideo = {
+    m87: "Zhx1n6uvgUE",
+    ireallywanttostayatyourhouse: "Rbgw_rduQpM",
+    GeorgeKarizakisNoRiderSystem: "YT8KbCj3_Gg",
+    volcano: "lNF9yf0gQsY",
+    TRUSTLAST: "xpN7rNcm5Vc",
+    Cherryish: "COMmXh8FkyI",
+    TsumitoBatsutoAngura: "R6Nutig1Awo",
+    Kaibutsu: "dy90tA3TT1c",
+    GenesisofAquarion: "qHxebm8sLt0",
+    OneLastKiss: "0Uhh62MUEic"
+}
+var listmusicnum = {
+    0: "m87",
+    1: "ireallywanttostayatyourhouse",
+    2: "GeorgeKarizakisNoRiderSystem",
+    3: "volcano",
+    4: "TRUSTLAST",
+    5: "Cherryish",
+    6: "TsumitoBatsutoAngura",
+    7: "Kaibutsu",
+    8: "GenesisofAquarion",
+    9: "OneLastKiss"
+}
